71426486a51869379b5d449b07a8fce82bb25b36
OnkarabileMasoko <156708064+OnkarabileMasoko@users.noreply.github.com>
2024-03-28 14:50:45 +0200

    c

--- Module_6_StudentNo_Classcode_Group_Name-Surname_JSL06-main/index.js
+++ Module_6_StudentNo_Classcode_Group_Name-Surname_JSL06-main/index.js
@@ -54,8 +54,7 @@ function addToOrder(itemName) {
 
     // Append the list item to the order items list
     orderList.appendChild(listItem);
-
-    // Calculate and update the total price (for demonstration, assuming all items have a price of 10)
+ 
     let totalPrice = parseFloat(orderTotal.textContent) || 0;
     totalPrice += 60; 
     orderTotal.textContent = totalPrice.toFixed(2); // Displaying total up to 2 decimal places
